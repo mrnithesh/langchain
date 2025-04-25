60cc98f362f0e02dcdee1b558abcacdfd5b2462e
mrnithesh <mr.nithesh.k@gmail.com>
2025-04-25 12:28:40 +0530

    minor code changes in the invoke method

--- basics.py
+++ basics.py
@@ -8,7 +8,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0, max_tokens=1000, api_key=GEMINI_API_KEY)
 
 def chat(user_input):
-    ai_response = llm.invoke(user_input)
+    ai_response = llm.invoke(HumanMessage(content = user_input))
     return ai_response.content
 
 print("Enter exit to end the chat")
