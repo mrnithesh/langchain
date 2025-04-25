8d40f65e53e04c588a70d1f0981e0703b078459b
mrnithesh <mr.nithesh.k@gmail.com>
2025-04-25 12:21:10 +0530

    implemented basic model invoke method

--- basics.py
+++ basics.py
@@ -7,5 +7,15 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0, max_tokens=1000, api_key=GEMINI_API_KEY)
 
-response = llm.invoke("What is the capital of France?")
-print(response.content)  
+def chat(user_input):
+    ai_response = llm.invoke(user_input)
+    return ai_response.content
+
+print("Enter exit to end the chat")
+
+while True:
+    user_input=input("User :")
+    if user_input.lower() == "exit":
+        print("Exiting!!")
+        break
+    print(chat(user_input))
